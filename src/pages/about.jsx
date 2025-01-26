4268e59eff9ba01d6c13f1865dc4ba681bd190c1
Sonal Sukanya <sonalsukanya205@gmail.com>
2025-01-26 16:31:28 +0530

    Initial commit

---
+++ src/pages/about.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets';
+import NewsletterBox from '../components/NewsletterBox';
+
+const about = () => {
+  return (
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+      <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+      <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+        <b className='text-gray-800'>Our Mission</b>
+        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
+      </div>
+      </div>
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+
+      </div>
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur. </p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gra'>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham. </p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gra'>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain... className='text-gra' </p>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
+  )
+}
+
+export default about
